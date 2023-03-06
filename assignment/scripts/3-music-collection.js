550c4620b3f97b143802d64f702e8192723bcc30
Andrew Harasymiw <aharasymiw@gmail.com>
2023-03-06 08:02:24 -0800

    create a function to take in info about an album, turn that info into an object, add the object to my collection, and return the object.

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,1 +1,21 @@
 console.log('***** Music Collection *****')
+
+// Create an empty collection array.
+let collection = [];
+
+// Take in information about an album, turn it into an object, and add it to our
+// collection array.
+function addToCollection(title, artist, yearPublished) {
+    // Turn our information into an object.
+    let newCollectionItem = {
+        title: title,
+        artist: artist,
+        yearPublished: yearPublished
+    };
+
+    // Add that object to our collection array.
+    collection.push(newCollectionItem)
+
+    // Return the object we created.
+    return newCollectionItem;
+}
